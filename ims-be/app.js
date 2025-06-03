9fcaf4b3bc1ccebca9935534bc38f9b75ac3ed75
krishna1911907 <krichauhan2005@gmail.com>
2025-06-03 21:55:36 +0530

    f_commit

--- ims-be/app.js
+++ ims-be/app.js
@@ -1,17 +1,36 @@
-const libExpress=require("express")
-const server=libExpress()
-server.post("/users",(req,res)=>{
-    console.log("received request")
-    res.send("user created")
+const libExpress = require("express")
+const cors = require("cors")
+const server = libExpress()
+server.use(cors())
+server.use(libExpress.json())
+const { MongoClient } = require("mongodb")
+const connection = new MongoClient("mongodb://krishna:krishna19@localhost:27017/IMS?authSource=IMS")
+
+server.post("/users", async (req, res) => {
+    console.log("user request")
+    if (req.body.name && req.body.email && req.body.password && req.body.phone) {
+        await connection.connect()
+        const db = await connection.db("IMS")
+        const collection = await db.collection("USERS")
+        const result = await collection.find({ "email": req.body.email }).toArray()
+
+        if (result.length > 0) {
+            res.json("User Already Exist!")
+
+
+        } else {
+            await collection.insertOne({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                phone: req.body.phone
+            })
+            res.json("USER CREATED SUCCESSFULLY!")
+
+        }
+    }
 })
-server.post("/players",(req,res)=>{
-    console.log("received request")
-    res.send("player created")
-})
-server.post("/team",(req,res)=>{
-    console.log("received request")
-    res.send("team created")
-})
-server.listen(8000,()=>{
+
+server.listen(8000, () => {
     console.log("server is ready on 8000")
 })
